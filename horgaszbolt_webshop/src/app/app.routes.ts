b816cb2eabb93244db35840572eec400f0d6e32f
csabszi <sz.csabi321@gmail.com>
2025-04-11 01:26:47 +0200

    basic routing and 404/not found page

--- horgaszbolt_webshop/src/app/app.routes.ts
+++ horgaszbolt_webshop/src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'product-list', component: ProductListComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '**', component: NotFoundComponent },
 ];
