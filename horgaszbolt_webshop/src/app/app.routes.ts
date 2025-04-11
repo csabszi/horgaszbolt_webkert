9476c76972cee8fa2c3a886d313562bff8db9d60
csabszi <sz.csabi321@gmail.com>
2025-04-11 21:27:56 +0200

    fixing home page landing

--- horgaszbolt_webshop/src/app/app.routes.ts
+++ horgaszbolt_webshop/src/app/app.routes.ts
@@ -6,10 +6,11 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'product-list', component: ProductListComponent },
     { path: 'contact', component: ContactComponent },
     { path: '**', component: NotFoundComponent },
-    { path: 'products', component: ProductListComponent },
-    { path: 'products/:id', component: ProductDetailsComponent },
+    { path: 'product-details', component: ProductListComponent },
+    { path: 'product-details/:id', component: ProductDetailsComponent },
 ];
