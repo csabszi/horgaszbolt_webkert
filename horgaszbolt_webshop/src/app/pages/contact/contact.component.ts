40964bb9482b7391419cbb072f4f681ab3673b31
csabszi <sz.csabi321@gmail.com>
2025-04-11 19:40:31 +0200

    forms

--- horgaszbolt_webshop/src/app/pages/contact/contact.component.ts
+++ horgaszbolt_webshop/src/app/pages/contact/contact.component.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrls: ['./contact.component.scss'],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressSpinnerModule
+  ]
 })
-export class ContactComponent {
 
+export class ContactComponent {
+  isLoading = false;
+  contactForm;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['']
+    });
+  }
+
+  send() {
+    if (this.contactForm.valid) {
+      this.isLoading = true;
+
+      setTimeout(() => {
+        alert('Üzenet elküldve!');
+        this.contactForm.reset();
+        this.isLoading = false;
+      }, 1500);
+    }
+  }
 }
