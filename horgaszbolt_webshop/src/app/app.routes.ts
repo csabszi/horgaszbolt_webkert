053a385c9f4512b4b6becef6347cd3c0a6c19470
csabszi <sz.csabi321@gmail.com>
2025-04-12 23:29:17 +0200

    delete product-details

--- horgaszbolt_webshop/src/app/app.routes.ts
+++ horgaszbolt_webshop/src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -11,6 +10,4 @@ export const routes: Routes = [
     { path: 'product-list', component: ProductListComponent },
     { path: 'contact', component: ContactComponent },
     { path: '**', component: NotFoundComponent },
-    { path: 'product-details', component: ProductListComponent },
-    { path: 'product-details/:id', component: ProductDetailsComponent },
 ];
